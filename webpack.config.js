02e5811d599f3d324c61a3ab0ba00e5dce0a9424
Julie Ye <oopsyeh056@gmail.com>
2022-05-09 16:36:47 +0800

    First commit: basic structure with settings of webpack, babel, firebase, packge.json.

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "main.bundle.[hash].js",
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, "public"),
+        },
+        port: 5000,
+        historyApiFallback: true,
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./static/base.html",      
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.[hash].css",
+        })
+    ],
+    module:{
+		rules:[
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader,"css-loader"]
+			},
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+                } 
+            }, 
+		],
+	},
+  };
+  
